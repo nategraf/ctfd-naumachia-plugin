880997f993217904ef63d10fb847b66a462250ce
Victor Graf <nategraf1@gmail.com>
2020-10-12 23:47:54 +0000

    update naumachia plugin for CTFd 3.1

--- __init__.py
+++ __init__.py
@@ -29,17 +29,14 @@ registrar_timeout = 10
 
 class NaumachiaChallengeModel(Challenges):
     __mapper_args__ = {'polymorphic_identity': 'naumachia'}
-    id = db.Column(None, db.ForeignKey('challenges.id'), primary_key=True)
+    id = db.Column(
+        db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE"), primary_key=True
+    )
     naumachia_name = db.Column(db.String(80))
 
-    def __init__(self, name, description, value, category, state, naumachia_name, type='naumachia'):
-        self.name = name
-        self.description = description
-        self.value = value
-        self.category = category
-        self.type = type
-        self.state = state
-        self.naumachia_name = naumachia_name
+    def __init__(self, *args, **kwargs):
+        super().__init__(**kwargs)
+        self.naumachia_name = kwargs["naumachia_name"]
 
 class NaumachiaChallenge(BaseChallenge):
     id = "naumachia"  # Unique identifier used to register challenges
@@ -55,52 +52,23 @@ class NaumachiaChallenge(BaseChallenge):
         'view': '/plugins/{0}/assets/view.js'.format(plugin_dirname),
     }
 
-    @staticmethod
-    def create(request):
-        """
-        This method is used to process the challenge creation request.
+    # Allows the default implementations of create and delete in BaseChallenge to work here.
+    challenge_model = NaumachiaChallengeModel
 
-        :param request:
-        :return:
-        """
-        data = request.form or request.get_json()
-
-        challenge = NaumachiaChallengeModel(**data)
-
-        db.session.add(challenge)
-        db.session.commit()
-
-        return challenge
-
-    @staticmethod
-    def read(challenge):
+    @classmethod
+    def read(cls, challenge):
         """
         This method is in used to access the data of a challenge in a format processable by the front end.
 
         :param challenge:
         :return: Challenge object, data dictionary to be returned to the user
         """
-        data = {
-            'id': challenge.id,
-            'name': challenge.name,
-            'value': challenge.value,
-            'description': challenge.description,
-            'category': challenge.category,
-            'naumachia_name': challenge.naumachia_name,
-            'state': challenge.state,
-            'max_attempts': challenge.max_attempts,
-            'type': challenge.type,
-            'type_data': {
-                'id': NaumachiaChallenge.id,
-                'name': NaumachiaChallenge.name,
-                'templates': NaumachiaChallenge.templates,
-                'scripts': NaumachiaChallenge.scripts,
-            }
-        }
+        data = super().read(challenge)
+        data['naumachia_name'] = challenge.naumachia_name
         return data
 
-    @staticmethod
-    def update(challenge, request):
+    @classmethod
+    def update(cls, challenge, request):
         """
         This method is used to update the information associated with a challenge. This should be kept strictly to the
         Challenges table and any child tables.
@@ -110,97 +78,12 @@ class NaumachiaChallenge(BaseChallenge):
         :return:
         """
         data = request.form or request.get_json()
+
         for attr, value in data.items():
             setattr(challenge, attr, value)
 
         db.session.commit()
         return challenge
-
-    @staticmethod
-    def delete(challenge):
-        """
-        This method is used to delete the resources used by a challenge.
-
-        :param challenge:
-        :return:
-        """
-        Fails.query.filter_by(challenge_id=challenge.id).delete()
-        Solves.query.filter_by(challenge_id=challenge.id).delete()
-        Flags.query.filter_by(challenge_id=challenge.id).delete()
-        files = ChallengeFiles.query.filter_by(challenge_id=challenge.id).all()
-        for f in files:
-            delete_file(f.id)
-        ChallengeFiles.query.filter_by(challenge_id=challenge.id).delete()
-        Tags.query.filter_by(challenge_id=challenge.id).delete()
-        Hints.query.filter_by(challenge_id=challenge.id).delete()
-        NaumachiaChallengeModel.query.filter_by(id=challenge.id).delete()
-        Challenges.query.filter_by(id=challenge.id).delete()
-        db.session.commit()
-
-    @staticmethod
-    def attempt(challenge, request):
-        """
-        This method is used to check whether a given input is right or wrong. It does not make any changes and should
-        return a boolean for correctness and a string to be shown to the user. It is also in charge of parsing the
-        user's input from the request itself.
-
-        :param challenge: The Challenge object from the database
-        :param request: The request the user submitted
-        :return: (boolean, string)
-        """
-        data = request.form or request.get_json()
-        submission = data['submission'].strip()
-        flags = Flags.query.filter_by(challenge_id=challenge.id).all()
-        for flag in flags:
-            if get_flag_class(flag.type).compare(flag, submission):
-                return True, 'Correct'
-        return False, 'Incorrect'
-
-    @staticmethod
-    def solve(user, team, challenge, request):
-        """
-        This method is used to insert Solves into the database in order to mark a challenge as solved.
-
-        :param team: The Team object from the database
-        :param chal: The Challenge object from the database
-        :param request: The request the user submitted
-        :return:
-        """
-        data = request.form or request.get_json()
-        submission = data['submission'].strip()
-        solve = Solves(
-            user_id=user.id,
-            team_id=team.id if team else None,
-            challenge_id=challenge.id,
-            ip=get_ip(req=request),
-            provided=submission
-        )
-        db.session.add(solve)
-        db.session.commit()
-        db.session.close()
-
-    @staticmethod
-    def fail(user, team, challenge, request):
-        """
-        This method is used to insert Fails into the database in order to mark an answer incorrect.
-
-        :param team: The Team object from the database
-        :param chal: The Challenge object from the database
-        :param request: The request the user submitted
-        :return:
-        """
-        data = request.form or request.get_json()
-        submission = data['submission'].strip()
-        wrong = Fails(
-            user_id=user.id,
-            team_id=team.id if team else None,
-            challenge_id=challenge.id,
-            ip=get_ip(request),
-            provided=submission
-        )
-        db.session.add(wrong)
-        db.session.commit()
-        db.session.close()
 
 def user_can_get_config():
     if is_admin():
